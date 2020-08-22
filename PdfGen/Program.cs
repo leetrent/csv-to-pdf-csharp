2081fdc6d7970dadda7c96f5f1bcc0efd9f40d8a
LeeTrent <lee.trent.1@gmail.com>
2020-08-22 16:44:06 -0500

    First commit - starting project

---
+++ PdfGen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace PdfGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logSnippet = "[Program][Main] => ";
+
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Environment.GetEnvironmentVariable("APPSETTINGS_DIRECTORY"))
+                .AddJsonFile("PdfGen.config.json", optional: false, reloadOnChange: true)
+                .Build();
+            string template = config["AppConfiguration:Templates:SF2806:Filename"];
+            Console.WriteLine(logSnippet + $"(template): '{template}'");
+        }
+    }
+}
