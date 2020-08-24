6ec9b5a54d8b4dcdcc612cf6b9bd02eced3e95a1
LeeTrent <lee.trent.1@gmail.com>
2020-08-24 11:26:16 -0500

    Adds PdfGenerator

---
+++ PdfGen/Utils/PdfGenerator.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using iText.Kernel.Pdf;
+using iText.Forms;
+using iText.Forms.Fields;
+using PdfGen.Models;
+
+namespace PdfGen.Utils
+{
+    public class PdfGenerator
+    {
+        dynamic _etlRecords; 
+        string _pdfTemplateFileName;
+
+        string _password;
+
+        public PdfGenerator(dynamic etlRecs, string templateFileName, string password)
+        {
+            _etlRecords = etlRecs;
+            _pdfTemplateFileName= templateFileName;
+            _password = password;
+        }
+
+        public byte[] GeneratePdf()
+        {
+            using (MemoryStream outerMemoryStream = new MemoryStream())
+            {
+                PdfWriter pdfWriter = new PdfWriter(outerMemoryStream).SetSmartMode(true);
+                PdfDocument pdfDoc = new PdfDocument(pdfWriter);
+                pdfDoc.InitializeOutlines();
+
+                 PdfDocument srcPdfDoc = null;
+
+                 foreach (var etlRec in _etlRecords)
+                 {
+                     byte[] formFieldsInRow = this.fillFormFieldsInRow(etlRec);
+                     MemoryStream innerMemoryStream = new MemoryStream(formFieldsInRow);;
+                     PdfReader pdfReader = new PdfReader(innerMemoryStream);
+                     srcPdfDoc = new PdfDocument(pdfReader);
+                     srcPdfDoc.CopyPagesTo(1, srcPdfDoc.GetNumberOfPages(), pdfDoc);
+                 }
+                 pdfDoc.Close();
+                 return outerMemoryStream.ToArray();
+            }
+        }
+
+        private byte[] fillFormFieldsInRow<T>(T rec)
+        {
+            //string logSnippet = "[PdfGenerator][fillFormFieldsInRow] => ";
+   
+            using (var memoryStream = new MemoryStream())
+            {
+                PdfReader pdfReader = new PdfReader(_pdfTemplateFileName, 
+                                        new ReaderProperties().SetPassword(Encoding.ASCII.GetBytes(_password))); //Iput
+                PdfWriter pdfWriter = new PdfWriter(memoryStream);
+                PdfDocument pdfDoc = new PdfDocument(pdfReader, pdfWriter);
+                PdfAcroForm pdfForm = PdfAcroForm.GetAcroForm(pdfDoc, true);
+                IDictionary<string, PdfFormField> pdfFormFields = pdfForm.GetFormFields();
+
+                PropertyInfo[] properties = (PropertyInfo[])typeof(T).GetProperties()
+                                            .Where(x => x.GetCustomAttributes(typeof(PdfFieldAttribute), true).Any());
+
+                foreach (var prop in properties)
+                {
+                    var attribute = prop.GetCustomAttribute<PdfFieldAttribute>();
+
+                    if (!pdfFormFields.TryGetValue(attribute.FieldName, out var pdfField))
+                    {
+                        continue;
+                    }
+                    pdfField.SetValue(prop.GetValue(rec)?.ToString() ?? String.Empty);
+                }
+                pdfForm.FlattenFields();
+                pdfDoc.Close();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
